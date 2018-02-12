f563bc3e3f924cfa876f7167a39ed9fa08a67072
Erik Schnetter <schnetter@gmail.com>
2018-02-11 22:18:13 -0500

    Generalize ChebFun implementation

--- lib/ChebFun.hs
+++ lib/ChebFun.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-module ChebFun(ChebFun(..)) where
+module ChebFun (ChebFun(..)) where
 
+import qualified Data.Vector as V
 import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Unboxed as U
 import qualified Test.QuickCheck as QC
 import Test.QuickCheck.Instances ()
-import Text.Read
 
 import Category
 import Chebyshev
+import Unboxed
 
 
 
@@ -18,31 +21,23 @@ import Chebyshev
 ncoeffs :: Int
 ncoeffs = 5
 
--- TODO: Store as b
-newtype ChebFun v a b = ChebFun (v Double)
-instance G.Vector v Double => Eq (ChebFun v a b) where
-    ChebFun cs1 == ChebFun cs2 = G.eq cs1 cs2
-instance G.Vector v Double => Ord (ChebFun v a b) where
-    compare (ChebFun cs1) (ChebFun cs2) = G.cmp cs1 cs2
-instance G.Vector v Double => Read (ChebFun v a b) where
-    readPrec = ChebFun <$> G.readPrec
-instance G.Vector v Double => Show (ChebFun v a b) where
-    showsPrec d (ChebFun cs) = G.showsPrec d cs
 
-instance (G.Vector v Double, QC.Arbitrary (v Double))
-        => QC.Arbitrary (ChebFun v a b) where
+
+newtype ChebFun v a b = ChebFun (v b)
+    deriving (Eq, Ord, Read, Show)
+
+instance (G.Vector v b, QC.Arbitrary b) => QC.Arbitrary (ChebFun v a b) where
     -- arbitrary = ChebFun <$> QC.arbitrary
     -- shrink (ChebFun cs) = ChebFun <$> QC.shrink cs
     arbitrary = ChebFun <$> G.generateM ncoeffs (const QC.arbitrary)
     shrink (ChebFun cs) = []
 
-eval :: (G.Vector v Double, Numeric a, Numeric b) => ChebFun v a b -> a -> b
-eval (ChebFun cs) x = realToFrac (chebyshev (realToFrac (compactify x)) cs)
+eval :: (G.Vector v b, Numeric a, Numeric b) => ChebFun v a b -> a -> b
+eval (ChebFun cs) x = chebyshev (realToFrac (compactify x)) cs
 
-approx :: (G.Vector v Double, Numeric a, Numeric b)
+approx :: (G.Vector v b, Numeric a, Numeric b)
           => Int -> (a -> b) -> ChebFun v a b
-approx n f =
-    ChebFun (chebyshevApprox n (realToFrac . f . uncompactify . realToFrac))
+approx n f = ChebFun (chebyshevApprox n (f . uncompactify . realToFrac))
 
 compactify :: Floating a => a -> a
 compactify x = (2 / pi) * atan x
@@ -50,9 +45,20 @@ compactify x = (2 / pi) * atan x
 uncompactify :: Floating a => a -> a
 uncompactify x = tan ((pi / 2) * x)
 
-instance G.Vector v Double => Function (ChebFun v) where
-    type FunCat (ChebFun v) = Numeric
+
+
+instance Function (ChebFun V.Vector) where
+    type FunCat (ChebFun V.Vector) = Numeric
     chase = eval
 
-instance G.Vector v Double => Discretization (ChebFun v) where
+instance Discretization (ChebFun V.Vector) where
+    discretize = approx ncoeffs
+
+
+
+instance Function (ChebFun U.Vector) where
+    type FunCat (ChebFun U.Vector) = Unboxed
+    chase = eval
+
+instance Discretization (ChebFun U.Vector) where
     discretize = approx ncoeffs
